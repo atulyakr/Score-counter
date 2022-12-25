ea21c47925405f03f82ced1926653a781783aa17
Atulya kumar <97295213+atulyakr@users.noreply.github.com>
2022-12-26 01:35:06 +0530

    Create score.js

---
+++ score.js
@@ -0,0 +1,34 @@
+
+let v_score=document.getElementById("v-score-btn")
+let z_score=document.getElementById("z-score-btn")
+function v_add1()
+{
+    
+    v_score.textContent=(v_score.textContent-'0')+1
+}
+
+function v_add2()
+{
+    v_score.textContent=v_score.textContent-'0'+2
+}
+
+function v_add3()
+{
+    v_score.textContent=v_score.textContent-'0'+3
+}
+
+function z_add1()
+{
+    z_score.textContent=(z_score.textContent-'0')+1
+}
+
+function z_add2()
+{
+    z_score.textContent=z_score.textContent-'0'+2
+}
+
+function z_add3()
+{
+    z_score.textContent=z_score.textContent-'0'+3
+}
+
